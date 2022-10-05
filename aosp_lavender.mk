71a5f6dd51454f07edaea41cb209640d7cb9f44e
Iris0411 <irisvyas@gmail.com>
2022-10-05 17:54:35 +0530

    lavender: PixelOS BringUp
    Signed-off-by: Iris0411 <irisvyas@gmail.com>

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -8,12 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common ProjectBlaze stuff
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
-BLAZE_BUILD_TYPE := OFFICIAL
-BLAZE_MAINTAINER := COPTAN
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USE_PIXEL_CHARGER := true
+# Inherit some common AOSP stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -21,7 +17,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := blaze_lavender
+PRODUCT_NAME := aosp_lavender
 PRODUCT_MODEL := Redmi Note 7
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
